c7d8d05dc4363db537496f9c848eb2f4ca74d948
ishawant kumar <ishawantkumar@gmail.com>
2024-01-31 16:58:26 +0530

    data ingestion  completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,23 +1,35 @@
 from src.logger.logging import logging 
 from src.exception.exception import customexception  
+from sklearn.model_selection import train_test_split
 
 import pandas as pd 
 import numpy as np 
 import os
 import sys 
-from pathlib import Path 
+from pathlib import Path  
+from dataclasses import dataclasse
 
 
+@dataclasse
 class DataIngestionConfig:
-    pass
+    raw_data_path:str = os.path.join("artifacts","raw.csv") 
+    train_data_path:str = os.path.join("artifacts","train.csv") 
+    test_data_path:str = os.path.join("artifacts","test.csv") 
+
+
 
 class DataIngestion:
     def __init__(self):
-        pass
+        self.ingestion_config=DataIngestionConfig() 
+
 
     def initiate_data_ingestion(self):
+        logging.info("Data ingestion started")
         try:
-            pass
+            data = pd.read_csv("ttps://raw.githubusercontent.com/ISHAWANT/Data_set/main/Gemstone_Price_Prediction.csv")
+            logging.info('reading a data frame') 
+
+            os.makedirs(os.path.join)
         except Exception as e:
             logging.info()
             raise customexception(e,sys)
