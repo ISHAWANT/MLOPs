08ea5ef057bb922c95c7ec29817eb52ae6ff28ca
ishawant kumar <ishawantkumar@gmail.com>
2024-01-31 17:15:56 +0530

    data ingestion  updated

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -16,8 +16,6 @@ class DataIngestionConfig:
     train_data_path:str = os.path.join("artifacts","train.csv") 
     test_data_path:str = os.path.join("artifacts","test.csv") 
 
-
-
 class DataIngestion:
     def __init__(self):
         self.ingestion_config=DataIngestionConfig() 
